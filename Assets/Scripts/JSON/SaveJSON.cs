80f20aa07f5a80d56cf67a2a95f50d84175ecff0
yussoba <juan97bp@gmail.com>
2022-10-10 18:29:52 -0300

    JSON ANASHE
    JSON ANASHE

---
+++ Assets/Scripts/JSON/SaveJSON.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveJSON : MonoBehaviour
+{
+    [SerializeField] SaveData data = new SaveData();
+
+    string path;
+
+    private void Awake()
+    {
+        path = Application.persistentDataPath + "/data.save";
+        Debug.Log(path);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SaveGame();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadGame();
+        }
+    }
+
+    public void SaveGame()
+    {
+        Debug.Log("Hola");
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(path, json);
+
+        
+        Debug.Log(json);
+    }
+
+    public void LoadGame()
+    {
+        string json = File.ReadAllText(path);
+        JsonUtility.FromJsonOverwrite(json, data);
+
+    }
+}
