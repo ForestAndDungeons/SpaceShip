37e8d7c70b5b62ef89c94c6b8799e3a3fe32df1e
yussoba <juan97bp@gmail.com>
2022-11-30 14:40:15 -0300

    Notifications 0.1
    Notifications Ready
    Falta buildear y testear

---
+++ Assets/Scripts/Notifications/NotificationsManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Notifications.Android;
+using System;
+public class NotificationsManager : MonoBehaviour
+{
+    private void Start()
+    {
+        AndroidNotificationCenter.CancelAllDisplayedNotifications();
+        AndroidNotificationCenter.CancelAllScheduledNotifications();
+
+        var notifChannel = new AndroidNotificationChannel()
+        {
+            Id = "reminder_notif_ch",
+            Name = "Reminder Notification",
+            Description = "Disturb the user",
+            Importance = Importance.High
+
+
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(notifChannel);
+
+        var notification = new AndroidNotification();
+        notification.Title = "Get your ship ready";
+        notification.Text = "¡We have a galaxy to save!";
+        notification.FireTime = DateTime.Now.AddSeconds(10);
+        notification.SmallIcon = "icon_reminders";
+
+        AndroidNotificationCenter.SendNotification(notification, "reminder_notif_ch");
+
+    }
+}
