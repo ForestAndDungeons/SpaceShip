40a9d10156409bdec70a0d16845226afa6431123
Ferrer_gustavo <ferrergustavo97@gmail.com>
2022-12-20 19:48:15 -0300

    SpaceShip v1.8.7
    -Support Grypo Button

--- Assets/Scripts/Screen Manage/ScreenOptions.cs
+++ Assets/Scripts/Screen Manage/ScreenOptions.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class ScreenOptions : ScreenUI
 {
+    [SerializeField] GameObject _supportGyroscope;
+    [SerializeField] GameObject _noSupportGyroscope;
+    private void Start()
+    {
+        if (GameManager.Instance.GetSupportGyro())
+        {
+            _noSupportGyroscope.SetActive(false);
+            _supportGyroscope.SetActive(true);
+        }
+        else
+        {
+            _noSupportGyroscope.SetActive(true);
+            _supportGyroscope.SetActive(false);
+        }
+    }
     public void BTN_Back() { 
         GameManager.Instance.GetScreenManager().PopScreen();
         GameManager.Instance.GetScreenManager().PushScreen("MenuPanel", transform.parent);
