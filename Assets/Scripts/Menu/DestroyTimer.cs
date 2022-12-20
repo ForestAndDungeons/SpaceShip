e7281c030f27a1ff3c61efbd2a53f13f0f9fdb88
Gonzalo <gonzalo.puertas@davinci.edu.ar>
2022-12-19 21:46:56 -0300

    SpaceShip v1.8.6
    - New instruction for gyro

--- Assets/Scripts/Menu/DestroyTimer.cs
+++ Assets/Scripts/Menu/DestroyTimer.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class DestroyTimer : MonoBehaviour
 {
     [SerializeField] float _timer;
+    [SerializeField] GameObject _joystick;
+    [SerializeField] GameObject _gyro;
 
     void Start()
     {
         DestroyOnTime();
+        
+        if(GameManager.Instance.GetIsGyro())
+        { 
+            _joystick.SetActive(false);
+            _gyro.SetActive(true);
+        }
+        else
+        {
+            _joystick.SetActive(true);
+            _gyro.SetActive(false);
+        }
     }
 
     public void DestroyOnTime()
